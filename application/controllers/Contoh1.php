49c061ab0a970205152a00f0912f36907dcc406c
ahmadzak0307 <ahmadzak0307@nusamandiri.ac.id>
2021-10-10 17:14:06 +0700

    menambahkan ke github cuk

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo "Nama saya Ahmad Zaky Yamani Saya tingal di daerah Ciputat olahraga yang saya suaki adalah Bulutanggkis";
+    }
+}
